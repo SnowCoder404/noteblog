c68d0933d764c54b38f1fd40d2468132d694aa7c
SnowCoder404 <lenschaumarco@protonmail.com>
2024-09-03 19:54:48 +0200

    First commit

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 if [ -d "./.git" ]; then 
-	read -p "Enter a commit Text: " commite
+	read -r -p "Enter a commit Text: " commite
 	git add .
-	git commit -m $commite
+	git commit -m "$commite"
 	git push
 else
 	read -p "Enter a commit Text: " commite
 	git init
 	git add .
-	git commit -m $commit
+	git commit -m "$commit"
 	git branch -M main
 	read -p "Enter a Github URL without https://: " url
 	git remote add origin https://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@$url
