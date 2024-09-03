5b9a987cf10c791c21aab535eca189dd28453094
SnowCoder404 <lenschaumarco@protonmail.com>
2024-09-03 23:38:07 +0200

    moveNotes funktion ias

--- scripts/script.js
+++ scripts/script.js
@@ -112,15 +112,6 @@ function deleteNote (index) {
     }
 }
 
-function moveToTrash (index) {
-    let trashNote = notes.splice(index, 1);
-    trashNotes.push(trashNote);
-    saveNotes('Aufgabe', notes);
-    saveNotes('Trash', trashNotes);
-    renderNotes();
-    renderTrashNotes();
-}
-
 function saveNotes (task, note) {
     let notesString = JSON.stringify(note) 
     localStorage.setItem(task, notesString); 
@@ -131,57 +122,15 @@ function getFromLocalData (localTask) {
     return JSON.parse(task);
 }
 
-function pushToArchiv (index) {
-    let archivNote = notes.splice(index, 1);
-    archivNotes.push(archivNote);
-    saveNotes('Aufgabe', notes);
-    saveNotes('Archiv', archivNotes);
-    renderNotes();
-    renderArchivNotes(); 
-}
-
-function archivToTrash(index) {
-    let trashNote = archivNotes.splice(index, 1);
-    trashNotes.push(trashNote);
-    saveNotes('Archiv', archivNotes);
-    saveNotes('Trash', trashNotes);
-    renderArchivNotes();
-    renderTrashNotes();
-    if (archivNotes.length == 0) {
-        document.getElementById('archiv').classList.remove('d_flex_c')
-    }
-}
-
-function trashToArchiv(index) {
-    let trashNote = trashNotes.splice(index, 1);
-    archivNotes.push(trashNote);
-    saveNotes('Archiv', notes);
-    saveNotes('Trash', trashNotes);
-    renderArchivNotes();
-    renderTrashNotes();
+function moveNotes(spliceNotes, index, pushNotes) {
+    let spliceNote = spliceNotes.splice[index, 1];
+    pushNotes.push(spliceNote);
+    saveAndRenderNotes();
     if (trashNotes.length == 0) {
         document.getElementById('trash').classList.remove('d_flex_c')
     }else if (archivNotes.length == 0) {
         document.getElementById('archiv').classList.remove('d_flex_c')
     }
-}
-
-function backToNotes(index) {
-    let backNote = archivNotes.splice(index, 1);
-    notes.push(backNote);
-    saveNotes('Archiv', archivNotes);
-    saveNotes('Aufgabe', notes);
-    renderNotes();
-    renderArchivNotes();
-    if (archivNotes.length == 0) {
-        document.getElementById('archiv').classList.remove('d_flex_c')
-    }
-}
-
-function moveNotes(spliceNotes, index, pushNotes) {
-    let spliceNote = spliceNotes.splice[index, 1];
-    pushNotes.push(spliceNote);
-    
 }
 
 function saveAndRenderNotes() {
